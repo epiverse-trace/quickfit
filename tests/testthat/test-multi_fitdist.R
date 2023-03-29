d0858833a0fe371d64c74ab56bda30d93dff6b87
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-03-29 14:29:06 +0100

    updated mutli_fitdist tests

--- tests/testthat/test-multi_fitdist.R
+++ tests/testthat/test-multi_fitdist.R
@@ -1,7 +1,13 @@
 test_that("multi_fitdist works as expected on vector", {
+  skip_if_not(
+    requireNamespace("fitdistrplus", quietly = TRUE),
+    message = "{fitdistrplus} package required for running test"
+  )
+
   res <- multi_fitdist(
     data = rlnorm(n = 100, meanlog = 1, sdlog = 1),
-    models = c("lnorm", "gamma", "weibull")
+    models = c("lnorm", "gamma", "weibull"),
+    func = fitdistrplus::fitdist
   )
   expect_s3_class(res, "data.frame")
   expect_identical(nrow(res), 3L)
@@ -9,11 +15,19 @@ test_that("multi_fitdist works as expected on vector", {
 })
 
 test_that("multi_fitdist works as expected on censored data", {
-  # data from fitdistrplus
-  data("salinity", package = "fitdistrplus")
+  skip_if_not(
+    requireNamespace("fitdistrplus", quietly = TRUE),
+    message = "{fitdistrplus} package required for running test"
+  )
+
+  data <- data.frame(
+    left = c(3.2, 20, 30, 20, 25, 0.23, 20, 12.80, 3.2, 25),
+    right = c(NA, 25, NA, NA, 47, NA, NA, 15, 47, NA)
+  )
   res <- multi_fitdist(
-    data = salinity,
-    models = c("lnorm", "gamma", "weibull")
+    data = data,
+    models = c("lnorm", "gamma", "weibull"),
+    func = fitdistrplus::fitdistcens
   )
   expect_s3_class(res, "data.frame")
   expect_identical(nrow(res), 3L)
@@ -21,11 +35,27 @@ test_that("multi_fitdist works as expected on censored data", {
 })
 
 test_that("multi_fitdist works as expected on vector", {
-  # data from coarseDataTools
-  data("nycH1N1", package = "coarseDataTools")
+  skip_if_not(
+    requireNamespace("coarseDataTools", quietly = TRUE),
+    message = "{coarseDataTools} package required for running test"
+  )
+
+  data <- data.frame(
+    EL = c(7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25,
+           7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25,
+           7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25, 7.25),
+    ER = c(12, 11, 10, 12, 11, 13, 11, 13, 11, 12, 11, 11, 11, 14, 14, 10,
+           10, 14, 11, 12, 12, 11, 10, 12, 11, 13, 11, 13, 11, 12),
+    SL  = c(11, 10, 9, 11, 10, 12, 10, 12, 10, 11, 10, 10, 10, 13, 13, 9,
+            9, 13, 10, 11, 11, 10, 9, 11, 10, 12, 10, 12, 10, 11),
+    SR = c(12, 11, 10, 12, 11, 13, 11, 13, 11, 12, 11, 11, 11, 14, 14, 10,
+           10, 14, 11, 12, 12, 11, 10, 12, 11, 13, 11, 13, 11, 12),
+    type = rep(0, 30)
+  )
   res <- multi_fitdist(
-    data = nycH1N1,
-    models = c("lnorm", "weibull")
+    data = data,
+    models = c(dist = "L", dist = "W"), # nolint
+    func = coarseDataTools::dic.fit
   )
   expect_s3_class(res, "data.frame")
   expect_identical(nrow(res), 2L)
