9447550e21d7241ce41fe2fcf63f1d398ad9761f
pratikunterwegs <pratikgupte16@gmail.com>
2024-01-30 17:17:32 +0000

    Rename all fn to func
    Update documentation

--- R/function_checkers.R
+++ R/function_checkers.R
@@ -14,18 +14,18 @@
 #' input returns a numeric vector output of the same length as the input, with
 #' finite non-missing values \eqn{\geq} 0.0.
 #'
-#' @param fn A function. This is expected to be a function evaluating the
+#' @param func A function. This is expected to be a function evaluating the
 #' density of a distribution at numeric values.
 #' @param n_req_args The number of required arguments, i.e., arguments without
 #' default values.
-#' @param n The number of elements over which to evaluate the function `fn`.
-#' Defaults to 10, and `fn` is evaluated over `seq(n)`.
+#' @param n The number of elements over which to evaluate the function `func`.
+#' Defaults to 10, and `func` is evaluated over `seq(n)`.
 #'
-#' @return A logical for whether the function `fn` fulfils conditions specified
-#' in the respective checks.
+#' @return A logical for whether the function `func` fulfils conditions
+#' specified in the respective checks.
 #' @keywords internal
-test_fn_req_args <- function(fn, n_req_args = 1) {
-  # NOTE: replacing checkmate::assert_count()
+test_func_req_args <- function(func, n_req_args = 1) {
+  # check for a non-negative integerish value
   stopifnot(
     "`n_req_args` must be a single number" = (is.numeric(n_req_args)) &&
       (length(n_req_args) == 1),
@@ -34,31 +34,31 @@ test_fn_req_args <- function(fn, n_req_args = 1) {
     "`n_req_args` must be a natural number > 0" = (n_req_args > 0) &&
       (n_req_args %% 1 == 0)
   )
-  # NOTE: using formals(args(fn)) to allow checking args of builtin primitives
-  # for which formals(fn) would return NULL and cause the check to error
+  # NOTE: using formals(args(func)) to allow checking args of builtin primitives
+  # for which formals(func) would return NULL and cause the check to error
   # NOTE: errors non-informatively for specials such as `if`
-  is.function(fn) &&
+  is.function(func) &&
     Reduce(
       x = Map(
         function(x, y) {
           is.name(x) && y != "..."
         },
-        formals(args(fn)), names(formals(args(fn)))
+        formals(args(func)), names(formals(args(func)))
       ),
       f = `+`
     ) == n_req_args
 }
 
 #' @name function_checkers
-test_fn_num_out <- function(fn, n = 10) {
-  # NOTE: replacing checkmate::assert_count()
+test_func_num_out <- function(func, n = 10) {
+  # check for a non-negative integerish value
   stopifnot(
     "`n` must be a single number" = (is.numeric(n)) && (length(n) == 1),
     "`n` must be finite and non-missing" = (is.finite(n)) && (!is.na(n)),
     "`n` must be a natural number > 0" = (n > 0) && (n %% 1 == 0),
-    "`fn` must be a function" = is.function(fn)
+    "`func` must be a function" = is.function(func)
   )
-  out <- fn(seq(n))
+  out <- func(seq(n))
 
   # return logical output of conditions
   is.numeric(out) && (length(out) == n) && (!anyNA(out)) &&
