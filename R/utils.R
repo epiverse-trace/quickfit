d2547e69f71d085b11f06316aff77fdad2a0023f
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-01-30 15:01:05 +0000

    added ic_tbl function

--- R/utils.R
+++ R/utils.R
@@ -78,3 +78,72 @@
   # return estimate
   prob_est
 }
+
+#' Helper function to create a model comparison table
+#'
+#' @description This is a helper function for creating a model comparison
+#' `<data.frame>` primarily for use in the \pkg{superspreading} vignettes. It
+#' is designed specifically for handling [fitdistrplus::fitdist()] output and
+#' not a generalised function. See [bbmle::ICtab()] for a more general use
+#' function to create information criteria tables.
+#'
+#' @param ... [dots] One or more model fit results from
+#' [fitdistrplus::fitdist()].
+#' @param sort_by A `character` string specifying which information criterion
+#' to order the table by, either `"AIC"` (default), `"BIC"`, or `"none"`
+#' (i.e. no ordering).
+#'
+#' @return A `<data.frame>`.
+#' @export
+#' @examples
+#' if (requireNamespace("fitdistrplus", quietly = TRUE)) {
+#'   cases <- rnbinom(n = 100, mu = 5, size = 0.7)
+#'   pois_fit <- fitdistrplus::fitdist(data = cases, distr = "pois")
+#'   geom_fit <- fitdistrplus::fitdist(data = cases, distr = "geom")
+#'   nbinom_fit <- fitdistrplus::fitdist(data = cases, distr = "nbinom")
+#'   ic_tbl(pois_fit, geom_fit, nbinom_fit)
+#' }
+ic_tbl <- function(..., sort_by = c("AIC", "BIC", "none")) {
+  sort_by <- match.arg(sort_by)
+  models <- list(...)
+
+  # input checking
+  stopifnot(
+    "Input objects must be <fitdist>" =
+      vapply(
+        models,
+        inherits,
+        FUN.VALUE = logical(1),
+        what = "fitdist"
+      )
+  )
+
+  distribution <- vapply(models, "[[", FUN.VALUE = character(1), "distname")
+  aic <- vapply(models, "[[", FUN.VALUE = numeric(1), "aic")
+  bic <- vapply(models, "[[", FUN.VALUE = numeric(1), "bic")
+
+  delta_aic <- aic - min(aic)
+  delta_bic <- bic - min(bic)
+
+  aic_weight <- exp((-delta_aic) / 2) / sum(exp((-delta_aic) / 2))
+  bic_weight <- exp((-delta_bic) / 2) / sum(exp((-delta_bic) / 2))
+
+  model_tbl <- data.frame(
+    distribution = distribution,
+    AIC = aic,
+    DeltaAIC = delta_aic,
+    wAIC = aic_weight,
+    BIC = bic,
+    DeltaBIC = delta_bic,
+    wBIC = bic_weight
+  )
+
+  if (sort_by != "none") {
+    model_tbl <- model_tbl[order(model_tbl[[sort_by]]), ]
+    row.names(model_tbl) <- NULL
+  }
+
+
+  # return tbl
+  model_tbl
+}
